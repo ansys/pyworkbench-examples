c7eb76f5f7f78afd4e929837128ed36d90b288fb
Jorge Martinez <jorge.martinezgarrido@ansys.com>
2024-05-24 14:52:12 +0200

    fix: remove jupytext metadata

--- examples/demo_pywb_with_pymech/demo.py
+++ examples/demo_pywb_with_pymech/demo.py
@@ -1,16 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.16.2
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
 
 # # PyWorkbench and PyMechanical
 
--- examples/example_05_pywb_pyfluent_mixing_elbow/pyfluent_mixing_elbow.py
+++ examples/example_05_pywb_pyfluent_mixing_elbow/pyfluent_mixing_elbow.py
@@ -1,17 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.16.2
-#   kernelspec:
-#     display_name: 'Python 3.8.10 (''pywb_env'': venv)'
-#     language: python
-#     name: python3
-# ---
-
 # # Fluent setup and solution using PyFluent APIs
 # ------------------------------------------------
 # This example sets up and solves a three-dimensional turbulent fluid flow
