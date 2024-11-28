c38cb90dd0fb37e9e7a4637dd43afdc4b0e44369
Jorge Martinez <jorge.martinezgarrido@ansys.com>
2024-11-28 16:13:17 +0100

    dbg: ignore aedt example

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -44,7 +44,7 @@ extensions = [
 
 
 templates_path = ['_templates']
-exclude_examples = ["grantami-integration"]
+exclude_examples = ["grantami-integration", "ansys-aedt-workflow"]
 exclude_patterns = [
     "conf.py",
     "examples/**/scripts/*.py",
