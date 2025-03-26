77ca8a86da4f1dbf9df037903bd2cf0142cb0d14
Muhammed Adedigba <muhammed.adedigba@ansys.com>
2025-03-26 14:39:05 +0100

    fix: test only pyfluent-workflow

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -45,7 +45,17 @@ extensions = [
 
 
 templates_path = ['_templates']
-exclude_examples = ["grantami-integration", "ansys-aedt-workflow"]
+exclude_examples = [
+    "grantami-integration",
+    "ansys-aedt-workflow",
+    "ansys-fluent-workflow",
+    "axisymmetric-rotor",
+    "cooled-turbine-blade",
+    "cyclic-symmetry-analysis",
+    "logging",
+    "material-designer-workflow",
+    "pymechanical-integration",
+]
 exclude_patterns = [
     "conf.py",
     "examples/**/scripts/*.py",
